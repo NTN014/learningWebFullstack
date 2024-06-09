124ec7eab2e1ea0df51d0f6fe4b85438685fa9ae
binn <nguyenbinkuten@gmail.com>
2024-06-09 16:24:26 +0700

    second commit

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,7 +1,19 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('database', 'username', 'password', {
+const sequelize = new Sequelize('binn', 'root', null, {
   host: 'localhost',
-  dialect: 'mysql' 
+  dialect: 'mysql',
+  logging: false,
 });
+
+let connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+module.exports = connectDB;
